3c7849ecaea5efabf4638a11d564d4ce7afdede1
DaisiSegun <daissegun096@gmail.com>
2024-05-04 11:35:42 +0100

    Your commit message

--- src/components/Sorry/Sorry.jsx
+++ src/components/Sorry/Sorry.jsx
@@ -7,7 +7,7 @@ function Sorry() {
   
   return (
     <div className='sorry-container1'>
-      <h1 className='sorry-header'>This Location is not available yet</h1>
+      <h1 className='sorry-header'>This Location is not available yet.</h1>
       
       
 
